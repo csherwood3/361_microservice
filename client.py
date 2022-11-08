50322e49da3675ba8c632e836706b9bd9ec360f7
csherwood3 <71786657+csherwood3@users.noreply.github.com>
2022-11-07 16:21:37 -0800

    Update client.py
    Added local socket functionality. Added a function header for the main function.

--- client.py
+++ client.py
@@ -2,24 +2,38 @@ import zmq
 
 
 def convert_to_kilometers(miles):
+    """
+    This client function takes miles as a parameter and converts it to kilometers.
+
+    A ZMQ socket is first created. Then, we connect to the server socket and send
+    the request from miles to kilometers. We get the response in bytes and decode
+    it. The value conversion is returned.
+    
+    If you want to use the Google socket, un-comment the Google connection line 
+    and comment out the local server connection.
+    """
     context = zmq.Context()
 
     # Socket to talk to server
     print("Connecting to miles/kilometer converter…")
     socket = context.socket(zmq.REQ)
-    socket.connect("tcp://34.127.106.38:3389")
+
+    # Local server connection
+    socket.connect("tcp://127.0.0.1:3389")
+
+    # Google server connection
+    # socket.connect("tcp://34.127.106.38:3389")
 
     # Send the request
-    request = miles
-    print(f"Sending request {request} …")
-    request = f"{request}".encode()
+    print(f"Sending request {miles} …")
+    request = f"{miles}".encode()
     socket.send(request)
 
     # Get the reply
-    message = socket.recv()
-    return message.decode()
+    kilometers = socket.recv()
+    socket.close()
+    return kilometers.decode()
 
 
 if __name__ == "__main__":
-    # Convert 300 miles to kilometers as an example
-    print(convert_to_kilometers(300))
+    print(convert_to_kilometers(23.2))
